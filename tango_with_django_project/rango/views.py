86e3b37dd68c9dc1b2e472e1ebc396d7ce93007a
Christopher Murnin <2511157m@student.gla.ac.uk>
2020-04-27 14:47:22 +0100

    Initial Commit

---
+++ tango_with_django_project/rango/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Construct a dictionary to pass to the template engine as its context.
+# Note the key boldmessage matches to {{ boldmessage }} in the template!
+# then
+# Return a rendered response to send to the client.
+# We make use of the shortcut function to make our lives easier.
+# Note that the first parameter is the template we wish to use.
+
+
+def index(request):
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    return render(request, 'rango/about.html')
+
+
+
